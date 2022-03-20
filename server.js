1020d1dac5a707deccec646107d18fae4b7d3597
nova557 <71886498+nova557@users.noreply.github.com>
2022-03-19 21:24:10 -0400

    Add Basic Web Server with HTML Files

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const http = require("http");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content_type", "text/html");
-  res.end("<h1>Hello World</h1>");
+
+  let path = "./";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
 });
 
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
